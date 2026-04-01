8922fb59413e835feb9d4ee69f2455e8153ecd93
joangavelan <joangavelan@gmail.com>
2026-04-01 14:02:30 -0500

    feat: add cmd+number buffer switching and close/reopen keybindings

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -10,8 +10,8 @@ return {
 		event = "VeryLazy",
 		opts = {
 			options = {
-				diagnostics = "nvim_lsp", -- show LSP errors in tabs
-				show_buffer_close_icons = false, -- cleaner look
+				diagnostics = "nvim_lsp",
+				show_buffer_close_icons = false,
 				show_close_icon = false,
 				separator_style = "thin",
 				offsets = {
@@ -24,10 +24,40 @@ return {
 				},
 			},
 		},
-		keys = {
-			{ "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
-			{ "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
-			{ "<leader>bd", "<cmd>bdelete<cr>", desc = "Close buffer" },
-		},
+		config = function(_, opts)
+			require("bufferline").setup(opts)
+
+			-- Cmd+number to jump to buffer (via Ghostty ESC sequences)
+			for i = 1, 9 do
+				vim.keymap.set("n", "<M-" .. i .. ">", function()
+					require("bufferline").go_to(i, true)
+				end, { desc = "Go to buffer " .. i })
+			end
+
+			-- Cmd+W to close buffer
+			vim.keymap.set("n", "<M-w>", "<cmd>bd<cr>", { desc = "Close buffer" })
+
+			-- Track closed buffers for reopen
+			local closed_buffers = {}
+
+			vim.api.nvim_create_autocmd("BufDelete", {
+				callback = function(ev)
+					local name = vim.api.nvim_buf_get_name(ev.buf)
+					if name ~= "" then
+						table.insert(closed_buffers, name)
+					end
+				end,
+			})
+
+			-- Cmd+Shift+W to reopen last closed buffer
+			vim.keymap.set("n", "<M-T>", function()
+				if #closed_buffers == 0 then
+					vim.notify("No recently closed buffers", vim.log.levels.INFO)
+					return
+				end
+				local last = table.remove(closed_buffers)
+				vim.cmd("edit " .. vim.fn.fnameescape(last))
+			end, { desc = "Reopen last closed buffer" })
+		end,
 	},
 }
