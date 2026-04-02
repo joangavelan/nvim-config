3cd8c98ff6ec5cf1a7ea11c9ad52e2aae4d52a22
joangavelan <joangavelan@gmail.com>
2026-04-02 01:08:14 -0500

    chore: setup elixir Expert LSP

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -4,47 +4,55 @@
 
 -- keymaps when LSP attaches to a buffer
 vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(args)
-    local map = vim.keymap.set
-    local opts = { buffer = args.buf, silent = true }
+	callback = function(args)
+		local map = vim.keymap.set
+		local opts = { buffer = args.buf, silent = true }
 
-    -- navigation
-    map("n", "gd", vim.lsp.buf.definition,      vim.tbl_extend("force", opts, { desc = "Go to definition" }))
-    map("n", "gD", vim.lsp.buf.declaration,     vim.tbl_extend("force", opts, { desc = "Go to declaration" }))
-    map("n", "gr", vim.lsp.buf.references,      vim.tbl_extend("force", opts, { desc = "Find references" }))
-    map("n", "gi", vim.lsp.buf.implementation,  vim.tbl_extend("force", opts, { desc = "Go to implementation" }))
+		-- navigation
+		map("n", "gd", vim.lsp.buf.definition, vim.tbl_extend("force", opts, { desc = "Go to definition" }))
+		map("n", "gD", vim.lsp.buf.declaration, vim.tbl_extend("force", opts, { desc = "Go to declaration" }))
+		map("n", "gr", vim.lsp.buf.references, vim.tbl_extend("force", opts, { desc = "Find references" }))
+		map("n", "gi", vim.lsp.buf.implementation, vim.tbl_extend("force", opts, { desc = "Go to implementation" }))
 
-    -- docs & info
-    map("n", "<leader>h", vim.lsp.buf.hover,        vim.tbl_extend("force", opts, { desc = "Hover docs" }))
-    map("n", "<leader>sh", vim.lsp.buf.signature_help, vim.tbl_extend("force", opts, { desc = "Signature help" }))
+		-- docs & info
+		map("n", "<leader>h", vim.lsp.buf.hover, vim.tbl_extend("force", opts, { desc = "Hover docs" }))
+		map("n", "<leader>sh", vim.lsp.buf.signature_help, vim.tbl_extend("force", opts, { desc = "Signature help" }))
 
-    -- code actions
-    map("n", "<leader>rn", vim.lsp.buf.rename,       vim.tbl_extend("force", opts, { desc = "Rename symbol" }))
-    map("n", "<leader>ca", vim.lsp.buf.code_action,  vim.tbl_extend("force", opts, { desc = "Code actions" }))
+		-- code actions
+		map("n", "<leader>rn", vim.lsp.buf.rename, vim.tbl_extend("force", opts, { desc = "Rename symbol" }))
+		map("n", "<leader>ca", vim.lsp.buf.code_action, vim.tbl_extend("force", opts, { desc = "Code actions" }))
 
-    -- diagnostics
-    map("n", "<leader>d",  vim.diagnostic.open_float, vim.tbl_extend("force", opts, { desc = "Show diagnostic" }))
-    map("n", "[d",         vim.diagnostic.goto_prev,  vim.tbl_extend("force", opts, { desc = "Prev diagnostic" }))
-    map("n", "]d",         vim.diagnostic.goto_next,  vim.tbl_extend("force", opts, { desc = "Next diagnostic" }))
-  end,
+		-- diagnostics
+		map("n", "<leader>d", vim.diagnostic.open_float, vim.tbl_extend("force", opts, { desc = "Show diagnostic" }))
+		map("n", "[d", vim.diagnostic.goto_prev, vim.tbl_extend("force", opts, { desc = "Prev diagnostic" }))
+		map("n", "]d", vim.diagnostic.goto_next, vim.tbl_extend("force", opts, { desc = "Next diagnostic" }))
+	end,
 })
 
 -- lua_ls: recognize vim global so it stops warning in nvim config files
 vim.lsp.config("lua_ls", {
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { "vim" },
-      },
-    },
-  },
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim" },
+			},
+		},
+	},
+})
+
+-- Expert: official Elixir LSP
+vim.lsp.config("expert", {
+	cmd = { "expert", "--stdio" },
+	root_markers = { "mix.exs", ".git" },
+	filetypes = { "elixir", "eelixir", "heex" },
 })
 
 -- enable all servers
 vim.lsp.enable({
-  "ts_ls",
-  "cssls",
-  "tailwindcss",
-  "sqlls",
-  "lua_ls",
+	"expert",
+	"ts_ls",
+	"cssls",
+	"tailwindcss",
+	"sqlls",
+	"lua_ls",
 })
