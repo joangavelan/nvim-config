38bf8eb6b2bc65f6e519cf885c7cc5ae362cd4e1
joangavelan <joangavelan@gmail.com>
2026-04-05 12:20:30 -0500

    chore: migrate nvim-treesitter to main branch for neovim 0.12

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,48 +3,43 @@
 -- =========================
 
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		lazy = false,
-		config = function()
-			require("nvim-treesitter").install({
-				"lua",
-				"elixir",
-				"heex",
-				"eex",
-				"typescript",
-				"tsx",
-				"javascript",
-				"css",
-				"html",
-				"json",
-				"sql",
-				"markdown",
-				"bash",
-			})
+	"nvim-treesitter/nvim-treesitter",
+	branch = "main",
+	build = ":TSUpdate",
+	lazy = false,
+	main = "nvim-treesitter",
+	init = function()
+		-- Enable highlighting and indentation for all filetypes
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function()
+				pcall(vim.treesitter.start)
+				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+			end,
+		})
 
-			vim.api.nvim_create_autocmd("FileType", {
-				pattern = {
-					"lua",
-					"elixir",
-					"heex",
-					"eex",
-					"typescript",
-					"typescriptreact",
-					"javascript",
-					"javascriptreact",
-					"css",
-					"html",
-					"json",
-					"sql",
-					"markdown",
-					"bash",
-				},
-				callback = function()
-					vim.treesitter.start()
-				end,
-			})
-		end,
-	},
+		-- Install parsers
+		local ensureInstalled = {
+			"lua",
+			"elixir",
+			"heex",
+			"eex",
+			"typescript",
+			"tsx",
+			"javascript",
+			"css",
+			"html",
+			"json",
+			"sql",
+			"markdown",
+			"bash",
+		}
+
+		local alreadyInstalled = require("nvim-treesitter.config").get_installed()
+		local toInstall = vim.iter(ensureInstalled)
+			:filter(function(p)
+				return not vim.tbl_contains(alreadyInstalled, p)
+			end)
+			:totable()
+		require("nvim-treesitter").install(toInstall)
+	end,
 }
